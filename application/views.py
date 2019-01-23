d38918bf9cdad0b4aa8f01845ab274cdbc488641
Abdulaziz Adamu <ajitata6f@gmail.com>
2019-01-23 03:27:12 +0100

    created get_news() endpoint in the views.py file

--- application/views.py
+++ application/views.py
@@ -1,5 +1,27 @@
 from application import app
+import feedparser
+
+RSS_FEEDS = {"bbc":"http://feeds.bbci.co.uk/news/rss.xml",
+             "cnn":"http://rss.cnn.com/rss/edition.rss",
+             "fox":"http://feeds.foxnews.com/foxnews/latest",
+             "iol":"http://www.iol.co.za/cmlink/1.640"}
 
 @app.route("/")
-def get_news():
-    return "no news is good news"
+@app.route("/<application>")
+def get_news(publication):
+    feed = feedparser.parse(RSS_FEEDS[publication])
+    first_article = feed['entries'][1]
+
+    return """"<html>
+            <body>
+                <h1>BBC Headlines</h1>
+                <b>{0}</b><br>
+                <i>{1}</i><br>
+                <p>{2}</p><br>
+            </body>
+        </html>
+        """.format(first_article.get("title"), first_article.get("published"), first_article.get("summary"))
+
+
+
+
